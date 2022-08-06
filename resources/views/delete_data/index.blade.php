a6d027d3c7f7d294265bc3af3b8e59038656a5cc
auspicious <auspiciouscluster@gmail.com>
2022-08-06 16:23:04 +0530

    delete and merge process design

--- resources/views/delete_data/index.blade.php
+++ resources/views/delete_data/index.blade.php
@@ -5,7 +5,7 @@
     <div class="padding">
         @include('__global.loading')
         <form-truncate inline-template ref="roleForm" class="sub_content">
-            <form>
+            <form class="sub_content_custom_form">
                 <div>Are you sure want to delete data</div>
                 <button class="btn btn-fw success " type="button" @click="submit">Submit</button>
             </form>
