c7f218ccd71fdf140ee728a7fb3ed05a0724c23d
auspicious <auspiciouscluster@gmail.com>
2022-09-18 12:53:06 +0530

    fc and loss report

--- app/Http/Controllers/API/FcTransactionReportController.php
+++ app/Http/Controllers/API/FcTransactionReportController.php
@@ -50,7 +50,19 @@ class FcTransactionReportController extends Controller
             DB::raw("ROUND(SUM(bag_styles.quantity) - IFNULL((SELECT lst.total_loss_quantity FROM transaction lst WHERE lst.bag_id = bag.id ORDER BY lst.id DESC LIMIT 1), 0),3) as quantity"),
             //DB::raw("ROUND(SUM(bag_styles.weight),3) as weight"),
             //DB::raw("(SELECT IFNULL(sum(t1.total_receive_weight), 0) FROM transaction t1 WHERE t1.bag_id = bag.id AND t1.to_department_id=9 order by t1.id desc limit 1) as weight"),
-            DB::raw("CASE WHEN (SELECT IFNULL(sum(t1.total_receive_weight), 0) FROM transaction t1 WHERE t1.bag_id = bag.id AND t1.transaction_date >= '" . $from_date . "' AND t1.transaction_date <= '" . $to_date . "' AND t1.to_department_id=9 order by t1.id desc limit 1) > 0 THEN (SELECT IFNULL(sum(t1.total_receive_weight), 0) FROM transaction t1 WHERE t1.bag_id = bag.id AND t1.transaction_date >= '" . $from_date . "' AND t1.transaction_date <= '" . $to_date . "' AND t1.to_department_id=9 order by t1.id desc limit 1) ELSE ROUND(SUM(bag_styles.weight),3) END as weight"),
+            //DB::raw("CASE WHEN (SELECT IFNULL(sum(t1.total_receive_weight), 0) FROM transaction t1 WHERE t1.bag_id = bag.id AND t1.transaction_date >= '" . $from_date . "' AND t1.transaction_date <= '" . $to_date . "' AND t1.to_department_id=9 order by t1.id desc limit 1) > 0 THEN (SELECT IFNULL(sum(t1.total_receive_weight), 0) FROM transaction t1 WHERE t1.bag_id = bag.id AND t1.transaction_date >= '" . $from_date . "' AND t1.transaction_date <= '" . $to_date . "' AND t1.to_department_id=9 order by t1.id desc limit 1) ELSE ROUND(SUM(bag_styles.weight),3) END as weight"),
+            DB::raw("
+                IFNULL(
+                    (
+                        SELECT 
+                        IFNULL(t1.total_receive_weight, 0) 
+                        FROM transaction t1 
+                        WHERE t1.bag_id = bag.id 
+                        AND t1.to_department_id=9 
+                        ORDER BY t1.id DESC LIMIT 1
+                    )
+                , 0) as weight"
+            ),
             DB::raw("GROUP_CONCAT(bag_styles.style_id) as style"),
             DB::raw("GROUP_CONCAT(style.sku) sku")
         );
